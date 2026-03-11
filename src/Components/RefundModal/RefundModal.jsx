6d5702048f914492c07efb7d20fbaa61b9d0117c
TayeburRahman <tayebrayhan101+@gmail.com>
2026-03-11 14:11:10 +0600

    updates all

--- src/Components/RefundModal/RefundModal.jsx
+++ src/Components/RefundModal/RefundModal.jsx
@@ -39,10 +39,10 @@ const RefundModal = ({ openRefundModal, setRefundModal, refundValue }) => {
         <Modal open={openRefundModal} centered footer={false} onCancel={() => setRefundModal(false)}>
             <div className='text-center'>
                 <h1 className='text-xl font-medium '>Are you sure want to Refund?</h1>
-                <p className='mt-5'>Win Bid amount will be refund tot the user/partner</p>
+                <p className='mt-5'>Win Bid amount will be refunded to the user/partner</p>
                 <div className='space-x-5 mt-5'>
                     <button onClick={() => handleRefund()} className='bg-black text-white rounded-full px-8 py-2'>Refund</button>
-                    <button className='border rounded-full px-8 py-2' >cancel</button>
+                    <button onClick={() => setRefundModal(false)} className='border rounded-full px-8 py-2' >Cancel</button>
                 </div>
             </div>
         </Modal>
